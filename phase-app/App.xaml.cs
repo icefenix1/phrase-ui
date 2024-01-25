e4bd13a8723c04413ab85cd07fa7da2baeb10570
Peter Bezuidenhout <icefenix@outlook.com>
2024-01-26 01:21:40 +0200

    Add project files.

---
+++ phase-app/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace phase_app
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ phase-app/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace phase_app
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
