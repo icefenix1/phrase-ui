4d57bf43ab043566e8d89375f2a8ebdf331696f1
Peter Bezuidenhout <icefenix@outlook.com>
2024-01-29 00:07:44 +0200

    # Added Phrase functionality ## Added
    Calls to Phrase Controller to:
    
    * Get List of phrases
    * Add phrases
    
    ## Modified
    
    n/a
    
    ## Removed
    
    Removed Default Template files.

---
+++ phrase-web/Models/PhraseResult.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace phrase_web.Models
+{
+    public class PhraseResult
+    {
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+
+    }
+}
--- phrase-web/Program.cs
+++ phrase-web/Program.cs
@@ -8,8 +8,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddBlazorBootstrap();
-
 var http = new HttpClient()
 {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
